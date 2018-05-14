aea89780f8bc681c8bb29a136a0700f10160f762
kchin07 <kchin07@calpoly.edu>
2018-05-14 13:44:50 -0700

    Add files via upload

--- cpe329_asgn8/main.c
+++ cpe329_asgn8/main.c
@@ -6,6 +6,8 @@ char character;
 char lock;
 uint8_t countDigits;
 char array[4];
+char arrayIndex;
+uint16_t data;
 uint16_t multiplier;
 #define myCS BIT0
 #define GAIN BIT5
@@ -13,10 +15,17 @@ uint16_t multiplier;
 void main(void)
 {
     int i;
-    uint16_t data;
     countDigits = 0;
     lock = 0;
+    arrayIndex = 0;
+    data = 0;
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    P1->SEL0 |= BIT5|BIT6|BIT7; //eUSCI_B_SPI
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST; //put SPI into reset
+    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST|EUSCI_B_CTLW0_MST|EUSCI_B_CTLW0_SYNC|EUSCI_B_CTLW0_CKPL|UCSSEL_2|EUSCI_B_CTLW0_MSB;
+    EUSCI_B0->BRW = 0x02;
+    EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_SWRST;
+
     //configure UART pins
     P1->SEL0 |= BIT2|BIT3; //puts pins into UART mode
 
@@ -38,102 +47,48 @@ void main(void)
     P6->DIR |= myCS;
     P6->OUT |= myCS;
 
-    P2->DIR |= BIT0|BIT1|BIT2;
-    P2->OUT &= ~(BIT0|BIT1|BIT2);
-
-//            EUSCI_A0->TXBUF = 'H';
-//            while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//            EUSCI_A0->TXBUF = 'e';
-//            while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//            EUSCI_A0->TXBUF = 'l';
-//            while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//            EUSCI_A0->TXBUF = 'l';
-//            while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//            EUSCI_A0->TXBUF = 'o';
-
     __enable_interrupt();
 
     NVIC->ISER[0] = 1<< ((EUSCIA0_IRQn) & 31);
-//    while(1){
-//        EUSCI_A0->TXBUF = 'H';
-//        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//        EUSCI_A0->TXBUF = 'e';
-//        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//        EUSCI_A0->TXBUF = 'l';
-//        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//        EUSCI_A0->TXBUF = 'l';
-//        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//        EUSCI_A0->TXBUF = 'o';
-//    }
+
     while(1) {
-        if (character != -1) {
-            uint8_t hiByte, loByte;
-            if (character == '\n') {//if 'return'
-                data = 0;
-                if (countDigits == 1) {
-                    multiplier = 1;
-                }
-                else if (countDigits == 2) {
-                    multiplier = 10;
-                }
-                else if (countDigits == 3) {
-                    multiplier = 100;
-                }
-                else if (countDigits == 4) {
-                    multiplier = 1000;
-                }
-                for (i = 0; i < countDigits; i++) {
-                    data += (multiplier) * array[i];
-                    multiplier /= 10;
-                }
-                loByte = 0xFF & data;
-                hiByte = 0x0F & (data >> 8);
-                hiByte |= GAIN|SHUTDN;
-
-                P6->OUT &= ~myCS;
-                while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
-                EUSCI_B0->TXBUF = hiByte;
-                while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
-                EUSCI_B0->TXBUF = loByte;
-                while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
-                P6->OUT |= myCS;
-
-//                EUSCI_A0->TXBUF = 'H';
-//                while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//                EUSCI_A0->TXBUF = 'e';
-//                while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//                EUSCI_A0->TXBUF = 'l';
-//                while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//                EUSCI_A0->TXBUF = 'l';
-//                while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
-//                EUSCI_A0->TXBUF = 'o';
-
-                P2->OUT &= ~(BIT0|BIT2|BIT3);
-
-                countDigits = 0;
+        __disable_irq();
+        uint8_t hiByte, loByte;
+        if (lock == 1) {//if 'return'
+            data = 0;
+            if (countDigits == 1) {
+                multiplier = 1;
             }
-            else if (character > 47 && character < 58) {
-                if (countDigits == 0){
-                    P2->OUT &= ~(BIT0|BIT2|BIT3);
-                    P2->OUT |= BIT0;
-                }
-                else if (countDigits == 1){
-                    P2->OUT &= ~(BIT0|BIT2|BIT3);
-                    P2->OUT |= BIT1;
-                }
-                else if (countDigits == 2){
-                    P2->OUT &= ~(BIT0|BIT2|BIT3);
-                    P2->OUT |= BIT2;
-                }
-                else if (countDigits == 4){
-                    P2->OUT &= ~(BIT0|BIT2|BIT3);
-                    P2->OUT |= 0;
-                }
-
-                array[countDigits] = character - 48;
-                countDigits++;
+            else if (countDigits == 2) {
+                multiplier = 10;
             }
+            else if (countDigits == 3) {
+                multiplier = 100;
+            }
+            else if (countDigits == 4) {
+                multiplier = 1000;
+            }
+            for (i = 0; i < countDigits; i++) {
+                data += (multiplier) * array[i];
+                multiplier /= 10;
+            }
+
+            loByte = 0xFF & data;
+            hiByte = 0x0F & (data >> 8);
+            hiByte |= GAIN|SHUTDN;
+
+            P6->OUT &= ~myCS;
+            while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
+            EUSCI_B0->TXBUF = hiByte;
+            while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
+            EUSCI_B0->TXBUF = loByte;
+            while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
+            P6->OUT |= myCS;
+            countDigits = 0;
+            lock = 0;
         }
+
+        __enable_irq();
     }
 }
 
@@ -142,5 +97,12 @@ void EUSCIA0_IRQHandler(){
         while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));//if it's not an IFG or an IFG is still going, wait
         EUSCI_A0->TXBUF = EUSCI_A0->RXBUF; //transmit?
         character = EUSCI_A0->RXBUF;
+        if(character == 13){
+            lock = 1;
+        }
+        else if (character > 47 && character < 58) {
+            array[countDigits] = character - 48;
+            countDigits++;
+        }
     }
 }
