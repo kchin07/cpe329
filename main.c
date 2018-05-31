879d547a9135f0a694ce0159eefc726de9d23124
kchin07 <kchin07@calpoly.edu>
2018-05-31 13:22:22 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,98 +1,234 @@
 #include "msp.h"
-#include "LCD.h"
 #include "delay.h"
-#include <stdio.h>
+#include "turret.h"
 
-void main(void) {
-    int col,row,row_select=1;
+#define MAXRIGHT 16000
+#define NUMBER_TIMER_CAPTURES 20
 
-    LCD_INIT();
-    LCD_CMD(0x80);
-    P4->SEL0 &= ~(BIT0|BIT1|BIT2|BIT3|BIT4|BIT5|BIT6);
-    P4->SEL1 &= ~(BIT0|BIT1|BIT2|BIT3|BIT4|BIT5|BIT6);
+volatile uint16_t timerAcaptureValues[NUMBER_TIMER_CAPTURES] = {0};
+uint16_t timerAcapturePointer = 0;
 
-    P4->DIR = 0x78;//MAKE P4.3-P4.6 outputs, rest are inputs
-    P4->REN = 0x07;//make P4.0-P4.2 resistors enabled
-    P4->OUT = 0x07;//make P4.0-P4.2 resistors set as pull-up
+void uart_init() {
+    // configure UART pins
+    P1->SEL0 |= BIT2|BIT3; // puts pins into UART mode
+    P1->SEL1 |= ~(BIT2|BIT3);
+    // configure serial device, similar to EUSCBI
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // reset mode
+    EUSCI_A0->MCTLW = 0;
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST|EUSCI_A_CTLW0_SSEL__SMCLK;
+    // configure to use SMCLK
 
-    P2->SEL1 &= ~(BIT0|BIT1|BIT2);  /* configure P2.0-P2.2 as simple I/O */
-    P2->SEL0 &= ~(BIT0|BIT1|BIT2);
-    P2->DIR |= BIT0|BIT1|BIT2;          /* P2.0-P2.2 set as output pin */
-    delay_ms(1,FREQ_3MHz);
+    EUSCI_A0->BRW = 4;
+    EUSCI_A0->MCTLW = (14<<EUSCI_A_MCTLW_BRF_OFS) | EUSCI_A_MCTLW_OS16;
+    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);
+}
 
-    while (1) {
-        int i;
-        int num;
-        for (i = 0; i < 4; i++){
-                P4->DIR &= 0x78; /* disable all rows */
-                P4->DIR |= 0x08 << i; /* enable one row at a time */
-                P4->OUT &= ~(0x08 << i); /* drive the active row low */
-                delay_ms(1, FREQ_3MHz); /* wait for signal to settle */
-                col = P4->IN & 0x07; /* read all columns */
-                P4->OUT |= 0x08 << i; /* drive the active row high */
-                if (col != 0xF0) break; /* if one of the input is low, some key is pressed. */
-        }
+void write_to_terminal(char byte) {
+    EUSCI_A0->TXBUF = byte;
+    while(!(EUSCI_A0->IFG&EUSCI_A_IFG_TXIFG)); //add a delay
+    EUSCI_A0->TXBUF = EUSCI_A0->RXBUF;
+}
 
-        col = P4->IN & 0x07;
-        if (col == 0x06 || col == 0x05 || col == 0x03) {
-            if (i % 2 == 0) {//even
-                P2->OUT |= BIT0;//red
+char hexTable[16] = {'0','1','2','3','4','5','6','7','8','9',
+                     'a','b','c','d','e','f'};
+
+void set_cursor(uint8_t row, uint8_t col) {
+  uint8_t character;
+  write_to_terminal(27);//ESC
+  write_to_terminal('[');
+
+  character = row / 100;
+  write_to_terminal(character + 48);
+  row -= (row/100) * 100;
+  character = row / 10;
+  write_to_terminal(character + 48);
+  row -= (row/10) * 10;
+  character = row / 1;
+  write_to_terminal(character + 48);
+
+  write_to_terminal(';');
+
+  character = col / 100;
+  write_to_terminal(character + 48);
+  col -= (col/100) * 100;
+  character = col / 10;
+  write_to_terminal(character + 48);
+  col -= (col/10) * 10;
+  character = col / 1;
+  write_to_terminal(character + 48);
+
+  write_to_terminal('H');
+}
+
+void clear_screen() {
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('2');
+  write_to_terminal('J');
+
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('H');
+}
+
+void print_string(char* s) {
+  while (*s != 0) {
+    write_to_terminal((uint8_t)*s);
+    s++;
+  }
+}
+
+void print_int32(uint32_t i) {
+  uint32_t highestPowerOf10 = 1000000000;
+
+  if (i == 0) {
+    write_to_terminal((uint8_t)hexTable[i]);
+    return;
+  }
+
+  while (i / highestPowerOf10 == 0) {//find highestPowerOf10
+    highestPowerOf10 /= 10;
+  }
+
+  while (highestPowerOf10 > 0) {
+    write_to_terminal((uint8_t)hexTable[i/highestPowerOf10]);
+    i -= (i/highestPowerOf10) * highestPowerOf10;
+    highestPowerOf10 /= 10;
+  }
+}
+
+void erase_line() {
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('2');
+  write_to_terminal('K');
+}
+
+void set_color(uint8_t color) {
+  uint8_t character;
+  write_to_terminal(27);
+  write_to_terminal('[');
+
+  character = color / 10;
+  write_to_terminal(character+48);
+  character = color - ((color/10) * 10);
+  write_to_terminal(character+48);
+
+  write_to_terminal('m');
+}
+
+void timer_init() {
+}
+
+//2.7 - trigger, output
+//2.5 - echo, input
+
+void sensor_init() {
+    P4->SEL0 &= ~(BIT1|BIT2);
+    P4->SEL1 &= ~(BIT1|BIT2);
+    P4->DIR = BIT1;
+    TIMER32_1->LOAD = 24000000;
+    TIMER32_1->CONTROL = 0xC2;
+}
+
+uint32_t getDistance() {
+    uint32_t highTime, lowTime;
+    P4->OUT |= BIT1;
+    __delay_cycles(3000); // delay cycles for TTL
+    P4->OUT &= ~BIT1;
+
+    while ((P4->IN & BIT2) == 0); // while still input low
+    // now is high
+    highTime = TIMER32_1->VALUE;
+    while ((P4->IN & BIT2) == BIT2); // while still input high
+    //now is low
+    lowTime = TIMER32_1->VALUE;
+
+    while ((TIMER32_1->RIS & 1) == 0);
+    TIMER32_1->INTCLR = 0;
+
+    return (highTime - lowTime) / (24 * 58);
+}
+
+void main(void){
+
+    turret_init();
+
+    uart_init();
+
+    sensor_init();
+
+    uint32_t distanceCms;
+
+    /************************************testing***********************************************/
+
+//    motor_on();
+//
+//    delay_ms(3000, FREQ_24MHz);
+//
+//    motor_off();
+
+    /************************************************************************************/
+//    delay_ms(1000, FREQ_24MHz);
+//
+//    TIMER_A0->CCR[2] = SERVOCENTER + FOURTYDEGREES; //activate gun motor
+//
+//    delay_ms(1000, FREQ_24MHz);//wait for servo to rotate
+
+
+
+    int count = 0;
+    char direction = 0; //0 = left, 1 = right
+    int start = MAXLEFT;
+
+    int cycles = 0;
+    int iterations = 0;
+
+	while(iterations != 1){
+	    //distance detection
+	     distanceCms = getDistance();
+
+	     set_cursor(1,1);
+	     erase_line();
+
+	     print_string("centimeters: ");
+	     print_int32(distanceCms);
+
+	     if(distanceCms < 25){//
+	         while(distanceCms < 50){
+
+	             pull_trigger();
+
+//	             delay_ms(1000, FREQ_24MHz);//pause
+
+	             distanceCms = getDistance();
+	         }
+	     }
+	     else{
+            //switch direction
+            if(count == DEGREES180){
+                count = 0;
+                direction ^= 1;
+                cycles++;
             }
-            else {
-                P2->OUT |= BIT2;
+
+            //rotate 180 degrees
+            else{
+                if(direction == 0){
+                    start -= FIVEDEGREES;
+                }
+                else{
+                    start += FIVEDEGREES;
+                }
+                TIMER_A0->CCR[3] = start;
             }
-        }
-        else {
-            P2->OUT &= ~(BIT0|BIT1|BIT2);
+            count ++;
+    //	    delay_ms(100, FREQ_24MHz);//controls how fast the servo rotates
+	     }
+	}
 
-        }
-        /*if (i == 4) {
-            P2->OUT &= ~(BIT0|BIT1|BIT2);
-        }
-        else if(col == 0x06){
-            num = 3*i + 1;
-                P2->OUT |= num;
+//    TIMER_A0->CCR[2] = SERVOCENTER; //deactivate gun motor
+//
+//    delay_ms(1000, FREQ_24MHz);//wait for servo to rotate
 
-        }
-        else if (col == 0x05){
-            num = 3*i + 2;
-                P2->OUT |= num;
-        }
-        else if (col == 0x03){
-            num = 3*i + 2;
-                P2->OUT |= num;
-        }
-        else {
-                P2->OUT &= ~(BIT0|BIT1|BIT2);
-        }
-
-        /*col = P4->IN & 0x07; // read all columns
-
-        if (col == 0x06){// 4.0 column pressed
-            P2->OUT |= BIT0;
-            LCD_WRITE('A');
-        }
-        else if (col == 0x05){// 4.1 column pressed
-            P2->OUT |= BIT1;
-            LCD_WRITE('B');
-        }
-        else if (col == 0x03){// 4.2 column pressed
-            P2->OUT |= BIT2;
-            LCD_WRITE('C');
-        }
-        else {
-            P2->OUT &= ~(BIT0|BIT1|BIT2);
-        }*/
-
-        /*if (col == 0x06 || col == 0x05 || col == 0x03) {
-            P4->DIR &= 0x78;
-        }*/
-
-    }
-
-
-    LCD_INIT();
-    LCD_CMD(0x80);
-    LCD_WRITE('A');
-    while(1);
 }
