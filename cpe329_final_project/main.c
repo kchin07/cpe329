850c363dd8502b5652181f9f7fb78f71fd12bb1e
jnishiji <jnishiji@calpoly.edu>
2018-05-31 12:35:22 -0700

    Add files via upload

--- cpe329_final_project/main.c
+++ cpe329_final_project/main.c
@@ -1,107 +1,176 @@
 #include "msp.h"
-#include "delay.h"
 
-#define SERVOCENTER 16800
-#define MAXLEFT 17600
 #define MAXRIGHT 16000
-#define FIVEDEGREES 44
-#define DEGREES180 36
-#define LEFT45DEGREES 16450
-#define RIGHT45DEGREES 17200
-#define FOURTYDEGREES 355
+#define NUMBER_TIMER_CAPTURES 20
 
-void main(void){
+volatile uint16_t timerAcaptureValues[NUMBER_TIMER_CAPTURES] = {0};
+uint16_t timerAcapturePointer = 0;
+
+
+void uart_init() {
+    // configure UART pins
+    P1->SEL0 |= BIT2|BIT3; // puts pins into UART mode
+    P1->SEL1 |= ~(BIT2|BIT3);
+    // configure serial device, similar to EUSCBI
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // reset mode
+    EUSCI_A0->MCTLW = 0;
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST|EUSCI_A_CTLW0_SSEL__SMCLK;
+    // configure to use SMCLK
+
+    EUSCI_A0->BRW = 13;
+    EUSCI_A0->MCTLW = (73<<EUSCI_A_MCTLW_BRF_OFS) | EUSCI_A_MCTLW_OS16;
+    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);
+}
+
+void write_to_terminal(char byte) {
+    EUSCI_A0->TXBUF = byte;
+    while(!(EUSCI_A0->IFG&EUSCI_A_IFG_TXIFG)); //add a delay
+    EUSCI_A0->TXBUF = EUSCI_A0->RXBUF;
+}
+
+char hexTable[16] = {'0','1','2','3','4','5','6','7','8','9',
+                     'a','b','c','d','e','f'};
+
+void set_cursor(uint8_t row, uint8_t col) {
+  uint8_t character;
+  write_to_terminal(27);//ESC
+  write_to_terminal('[');
+
+  character = row / 100;
+  write_to_terminal(character + 48);
+  row -= (row/100) * 100;
+  character = row / 10;
+  write_to_terminal(character + 48);
+  row -= (row/10) * 10;
+  character = row / 1;
+  write_to_terminal(character + 48);
+
+  write_to_terminal(';');
+
+  character = col / 100;
+  write_to_terminal(character + 48);
+  col -= (col/100) * 100;
+  character = col / 10;
+  write_to_terminal(character + 48);
+  col -= (col/10) * 10;
+  character = col / 1;
+  write_to_terminal(character + 48);
+
+  write_to_terminal('H');
+}
+
+void clear_screen() {
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('2');
+  write_to_terminal('J');
+
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('H');
+}
+
+void print_string(char* s) {
+  while (*s != 0) {
+    write_to_terminal((uint8_t)*s);
+    s++;
+  }
+}
+
+void print_int32(uint32_t i) {
+  uint32_t highestPowerOf10 = 1000000000;
+
+  if (i == 0) {
+    write_to_terminal((uint8_t)hexTable[i]);
+    return;
+  }
+
+  while (i / highestPowerOf10 == 0) {//find highestPowerOf10
+    highestPowerOf10 /= 10;
+  }
+
+  while (highestPowerOf10 > 0) {
+    write_to_terminal((uint8_t)hexTable[i/highestPowerOf10]);
+    i -= (i/highestPowerOf10) * highestPowerOf10;
+    highestPowerOf10 /= 10;
+  }
+}
+
+void erase_line() {
+  write_to_terminal(27);
+  write_to_terminal('[');
+  write_to_terminal('2');
+  write_to_terminal('K');
+}
+
+void set_color(uint8_t color) {
+  uint8_t character;
+  write_to_terminal(27);
+  write_to_terminal('[');
+
+  character = color / 10;
+  write_to_terminal(character+48);
+  character = color - ((color/10) * 10);
+  write_to_terminal(character+48);
+
+  write_to_terminal('m');
+}
+
+void timer_init() {
+}
+
+//2.7 - trigger, output
+//2.5 - echo, input
+
+void sensor_init() {
+    P4->SEL0 &= ~(BIT1|BIT2);
+    P4->SEL1 &= ~(BIT1|BIT2);
+    P4->DIR = BIT1;
+    TIMER32_1->LOAD = 24000000;
+    TIMER32_1->CONTROL = 0xC2;
+}
+
+uint32_t getDistance() {
+    uint32_t highTime, lowTime;
+    P4->OUT |= BIT1;
+    __delay_cycles(3000); // delay cycles for TTL
+    P4->OUT &= ~BIT1;
+
+    while ((P4->IN & BIT2) == 0); // while still input low
+    // now is high
+    highTime = TIMER32_1->VALUE;
+    while ((P4->IN & BIT2) == BIT2); // while still input high
+    //now is low
+    lowTime = TIMER32_1->VALUE;
+
+    while ((TIMER32_1->RIS & 1) == 0);
+    TIMER32_1->INTCLR = 0;
+
+    return (highTime - lowTime) / (24 * 58);
+}
+
+void main() {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-    CS->KEY = CS_KEY_VAL; //unlocked, now can modify clock system
-    CS->CTL0 = 0; //reset clock tuning
-    CS->CTL0 = CS_CTL0_DCORSEL_0;//set the frequency to 1.5MHz, CS_CTL0_DCORSEL_4 for 24 MHz
-    CS->CTL1 |= CS_CTL1_DIVS_1; //div clk by 2, CS_CTL1_DIVS_5 to div by 32
+    CS->KEY = CS_KEY_VAL;
+    CS->CTL0 = 0;
+    CS->CTL0 = CS_CTL0_DCORSEL_4;
+    //CS->CTL1 = CS->CTL1 & ~(CS_CTL1_SELM_MASK | CS_CTL1_DIVM_MASK) | CS_CTL1_SELM_3;
+    CS->KEY = 0;
 
-    CS->KEY = 0; //lock clock system
+    uart_init();
 
-	//set P2.4 to PWM for TimerA
-	P2->SEL0 |= BIT4;
-	P2->SEL1 &= ~BIT4;
-    P2->DIR |= BIT4; //set P2.4 as output
+    sensor_init();
 
-    //set P2.6 to PWM for TimerA
-    P2->SEL0 |= BIT6;
-    P2->SEL1 &= ~BIT6;
-    P2->DIR |= BIT6; //set P2.6 as output
+    uint32_t distanceCms;
+    while (1) {
+        distanceCms = getDistance();
 
-    //set P2.5 to PWM for TimerA
-    P2->SEL0 |= BIT5;
-    P2->SEL1 &= ~BIT5;
-    P2->DIR |= BIT5; //set P2.5 as output
+        set_cursor(1,1);
+        erase_line();
 
-	//initiliaze TimerA in capture mode
-	TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_2; //set/reset mode, pulling the trigger
-    TIMER_A0->CCTL[2] = TIMER_A_CCTLN_OUTMOD_2; //set/reset mode, running the gun motor
-    TIMER_A0->CCTL[3] = TIMER_A_CCTLN_OUTMOD_2; //set/reset mode, rotating the gun
-	TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK, up mode
-            TIMER_A_CTL_MC__UP;
-	TIMER_A0->CCR[0] = 18000; // want a 24ms period, so 24ms/(1/(0.75*10^6)) = 18000
+        print_string("centimeters: ");
+        print_int32(distanceCms);
+    }
 
-    TIMER_A0->CCR[1] = MAXRIGHT; //duty cycle, start right
-    TIMER_A0->CCR[2] = SERVOCENTER; //duty cycle, start center
-    TIMER_A0->CCR[3] = MAXLEFT; //duty cycle, start left
-
-    delay_ms(1450, FREQ_1P5MHz);
-
-    TIMER_A0->CCR[2] = SERVOCENTER + FOURTYDEGREES; //activate gun motor
-
-    delay_ms(1450, FREQ_1P5MHz);//wait for servo to rotate
-
-    int count = 0;
-    char direction = 0; //0 = left, 1 = right
-    int start = MAXLEFT;
-
-    int cycles = 0;
-    int iterations = 0;
-
-	while(iterations != 1){
-
-	    //switch direction
-	    if(count == DEGREES180){
-	        count = 0;
-	        direction ^= 1;
-	        cycles++;
-	    }
-
-	    //date firing
-	    if (cycles == 3){
-            int dartCount = 0;
-            while(dartCount < 4){
-                TIMER_A0->CCR[1] = MAXLEFT; //pull trigger
-
-                delay_ms(1450, FREQ_1P5MHz);//wait for servo to rotate
-
-                TIMER_A0->CCR[1] = MAXRIGHT; //release trigger
-
-                delay_ms(1450, FREQ_1P5MHz);//wait for servo to rotate
-
-                dartCount++;
-            }
-            cycles = 0;
-            iterations = 1;
-	    }
-
-	    //rotate 180 degrees
-	    else{
-	        if(direction == 0){
-	            start -= FIVEDEGREES;
-	        }
-	        else{
-                start += FIVEDEGREES;
-	        }
-	        TIMER_A0->CCR[3] = start;
-	    }
-        count ++;
-	    delay_ms(100, FREQ_1P5MHz);//controls how fast the servo rotates
-	}
-
-    TIMER_A0->CCR[2] = SERVOCENTER; //deactivate gun motor
-
-    delay_ms(1450, FREQ_1P5MHz);//wait for servo to rotate
 }
